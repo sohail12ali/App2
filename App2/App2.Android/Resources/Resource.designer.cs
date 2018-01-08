48cd21e201ffb934c845fab22fe72f467e651b37
sohail ali <sohail@outlook.in>
2018-01-08 12:08:48 +0530

    List of images is showing now

--- App2/App2.Android/Resources/Resource.designer.cs
+++ App2/App2.Android/Resources/Resource.designer.cs
@@ -26,6 +26,8 @@ namespace App2.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::PCLStorage.Resource.String.ApplicationName = global::App2.Droid.Resource.String.ApplicationName;
+			global::PCLStorage.Resource.String.Hello = global::App2.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::App2.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3684,6 +3686,12 @@ namespace App2.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f09003f
+			public const int ApplicationName = 2131296319;
+			
+			// aapt resource value: 0x7f09003e
+			public const int Hello = 2131296318;
 			
 			// aapt resource value: 0x7f090015
 			public const int abc_action_bar_home_description = 2131296277;
--- App2/App2/View/OtherPages/CaptureImage.xaml.cs
+++ App2/App2/View/OtherPages/CaptureImage.xaml.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App2.Helpers;
+using System.Diagnostics;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using Plugin.Permissions;
-using App2.Helpers;
-using System.Diagnostics;
 using Plugin.Permissions.Abstractions;
+using PCLStorage;
+
+
 
 namespace App2.View.OtherPages
 {
@@ -22,16 +25,32 @@ namespace App2.View.OtherPages
         {
             InitializeComponent();
             BindingContext = this;
+
         }
 
-        Image image = new Image();
+        public static List<string> list = new List<string>();
+        //public static List<string> pathList = new List<string>();
+        public static List<string> nameList = new List<string>();
+
+        public static Image CreateImageLayout(string imageSourcePath)
+        {
+
+            Image image = new Image()
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Start,
+                BackgroundColor = Color.Transparent,
+                Scale = 0.9,
+                Source = ImageSource.FromFile(imageSourcePath)
+            };
+
+            return image;
+        }
 
         public async void OnCaptured(Object sender, EventArgs args)
         {
             try
             {
-
-
 
                 await CrossMedia.Current.Initialize();
 
@@ -41,41 +60,52 @@ namespace App2.View.OtherPages
                     return;
                 }
 
+                string imageName = $"Photo{GlobalData.claimName}{DateTime.Now.ToShortTimeString()}";
 
                 var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new
                     Plugin.Media.Abstractions.StoreCameraMediaOptions()
                 {
                     SaveToAlbum = false, // change to true for storing images in galary
-                    Directory = "Sample", // storing images to sample directory
-                    Name = $"Photo{ GlobalData.claimName }{DateTime.Now.ToShortTimeString()}", // file name for the image 
-                });
+                    Directory = "Sample", // storing images to sample directory/Folder
+                    Name = imageName, // file name for the image
 
+                });
+                
                 if (photo == null)
                     return;
 
-                await DisplayAlert("File Location", photo.Path, "OK");
+
+                list.Add(photo.Path);  // image path added in the list
+                //pathList.Add(photo.AlbumPath);
+                nameList.Add(imageName);
+
+                // await DisplayAlert("File Location", $"Path:{photo.Path} Name {imageName}", "OK"); // Only for checking names of the images
 
                 if (photo != null)
                 {
                     PhotoImage.Source = ImageSource.FromStream(() =>
                     {
                         var stream = photo.GetStream();
+                        //images.Add(stream.ToString());
                         photo.Dispose();
                         return stream;
                     });
                 }
 
+                if (list != null && list.Any())
+                {
+                    ScrollLayout.Children.Add(CreateImageLayout(list.Last()));
+                    
+                }
+
+                //ScrollLayout.Children.Add();
+
 
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Exception: " + ex);
 
             }
-
-
-
-
-        }
+        }      
     }
 }
